85037f7d45cd997829bd48ff6bb5f3e1dce8e30e
Skoumpou <skourina@gmail.com>
2020-10-18 18:27:56 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { WrapperModule } from './wrapper/wrapper.module';
+
+
+
 @NgModule({
   declarations: [
     AppComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    WrapperModule
   ],
   providers: [],
   bootstrap: [AppComponent]
